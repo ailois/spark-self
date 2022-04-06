3873c581b9e21bb8d5a5040a27a5a94ce462e890
ailois <belard@sina.com>
2022-04-06 20:49:40 +0800

    test no header file

---
+++ src/main/scala/com/ailois/jobs/NoHeaderTest.scala
@@ -0,0 +1,20 @@
+package com.ailois.jobs
+
+import com.ailois.executor.SparkInit
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
+object NoHeaderTest {
+
+  val spark: SparkSession = SparkInit.sparkInstance()
+  val sparkContext: SQLContext = spark.sqlContext
+
+  def main(args: Array[String]): Unit = {
+
+    // load path must contains file if hdfs-site.xml in the same folder
+    val data = spark.read.format("csv")
+      .option("header", value = true)
+      .option("inferSchema", value = true)
+      .load("file:/D:\\IdeaProjects\\spark-self\\src\\main\\resources\\test_no_header.txt")
+    data.show(false)
+  }
+}
