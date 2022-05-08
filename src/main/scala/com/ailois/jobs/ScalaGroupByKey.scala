6a23d3b2d1a90ba10e78e01502b4ef48f7e169a7
ailois <belard@sina.com>
2022-05-08 22:28:47 +0800

    add sort for scala groupByKey

--- src/main/scala/com/ailois/jobs/ScalaGroupByKey.scala
+++ src/main/scala/com/ailois/jobs/ScalaGroupByKey.scala
@@ -14,23 +14,26 @@ object ScalaGroupByKey {
   import sparkContext.implicits._
 
   def main(args: Array[String]): Unit = {
-    val originDF = mutable.Seq(("id_1", Map(1 -> 1)), ("id_1", Map(1 -> 2)), ("id_1", Map(2 -> 2))).
-      toDF("key", "value")
+
+    val originDF = mutable.Seq(("id_1", Map(1 -> 1)), ("id_1", Map(1 -> 2)), ("id_1", Map(2 -> 2)),
+                               ("id_2", Map(1 -> 2)), ("id_2", Map(1 -> 3)), ("id_2", Map(2 -> 1)))
+                          .toDF("key", "value")
+
     val schema = originDF.schema
     implicit val encoder: ExpressionEncoder[Row] = RowEncoder.apply(schema)
     val result = originDF.groupByKey(_.getString(0))(Encoders.STRING).mapGroups((key, values) => {
-      val maps = mutable.ArrayBuffer[Map[String, Int]]()
-      while(values.hasNext){
-//        maps += values.next().getAs("value").asInstanceOf[Map[String, Int]]
-        maps += values.next().getAs[Map[String, Int]]("value")
+      val maps = mutable.ArrayBuffer[Map[Int, Int]]()
+      while (values.hasNext) {
+        //        maps += values.next().getAs("value").asInstanceOf[Map[String, Int]]
+        maps += values.next().getAs[Map[Int, Int]]("value")
       }
-      val res = maps.flatMap(_.toMap).foldLeft(mutable.Map[String, Int]())((map, y) => {
+      val res = maps.flatMap(_.toMap).foldLeft(mutable.Map[Int, Int]())((map, y) => {
         map(y._1) = map.getOrElse(y._1, 0) + y._2
         map
-      })
+      }).toList.sortBy(_._1).toMap
       Row.fromSeq(Seq(key, res))
     })
-    result.show(false)
+    result.orderBy("key").show(false)
   }
 
 }
