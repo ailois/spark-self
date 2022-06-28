b2f51c3606803e73d2fb1cd7fa776467c00f6581
ailois <belard@sina.com>
2022-06-28 16:17:15 +0800

    ready for new test

---
+++ src/main/java/com/ailois/test/Test.java
@@ -0,0 +1,14 @@
+package com.ailois.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+    private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        logger.info("ready for new test !!!");
+    }
+
+}
