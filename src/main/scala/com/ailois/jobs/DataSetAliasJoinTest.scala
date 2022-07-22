d0adbc101d8ef3f27ff54aed709329d65c35ebd1
ailois <belard@sina.com>
2022-07-22 18:28:33 +0800

    test anti semi join

--- src/main/scala/com/ailois/jobs/DataSetAliasJoinTest.scala
+++ src/main/scala/com/ailois/jobs/DataSetAliasJoinTest.scala
@@ -11,10 +11,13 @@ object DataSetAliasJoinTest {
   import sparkContext.implicits._
 
   def main(args: Array[String]): Unit = {
-    val dataLeft = List(("a", 1.0), ("b", 2.0)).toDF("x", "y")
-    val dataRight = List(("a", 2.0), ("b", 3.0), ("c", 4.0)).toDF("x", "y")
+    val dataLeft = List(("a", 1.0), ("a", 3.0), ("b", 2.0), ("e", 5.0)).toDF("x", "y")
+    val dataRight = List(("a", 2.0), ("c", 4.0)).toDF("x", "y")
     dataLeft.as("l").join(dataRight.as("r"), Seq("x"), "left")
       .selectExpr("l.*", "r.y as z").show(false)
+    dataLeft.join(dataRight, Seq("x"), "left_anti").show(false)
+    dataLeft.join(dataRight, Seq("x"), "left_semi").show(false)
+    dataLeft.join(dataRight, Seq("x"), "inner").show(false)
   }
 
 }
