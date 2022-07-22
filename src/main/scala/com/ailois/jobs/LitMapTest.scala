37f1d4838a98a5b6d80727fe248c6eff02fd449f
ailois <belard@sina.com>
2022-07-22 22:25:19 +0800

    add typedLit test

---
+++ src/main/scala/com/ailois/jobs/LitMapTest.scala
@@ -0,0 +1,22 @@
+package com.ailois.jobs
+
+import com.ailois.executor.SparkInit
+import org.apache.spark.sql.functions.typedLit
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
+object LitMapTest {
+
+  val spark: SparkSession = SparkInit.sparkInstance()
+  val sparkContext: SQLContext = spark.sqlContext
+
+  import sparkContext.implicits._
+
+  def main(args: Array[String]): Unit = {
+
+    var mapDF = Map("a?" -> 1, "b?" -> 2, "c?" -> 3).toList.toDF("key", "value")
+    mapDF = mapDF.withColumn("map", typedLit[Map[String, Int]](Map("1" -> 1)))
+    mapDF.show(false)
+
+  }
+
+}
