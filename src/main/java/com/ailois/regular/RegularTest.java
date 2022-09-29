21861e46833f3480bd70dc0bc23fccf9aca0a44f
ailois <belard@sina.com>
2022-09-29 12:28:01 +0800

    regex test

--- src/main/java/com/ailois/regular/RegularTest.java
+++ src/main/java/com/ailois/regular/RegularTest.java
@@ -1,16 +1,38 @@
 package com.ailois.regular;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class RegularTest {
 
     private static final Logger logger = LoggerFactory.getLogger(RegularTest.class);
 
     public static void main(String[] args) {
+
+        String test = "222, 22,22";
+        Arrays.stream(test.split(", |,")).collect(Collectors.toList()).forEach(logger::info);
+
+        String test2 = "(\"12(1)2\", \"2323\")";
+        List<String> parenthesesRemove = Arrays.stream(test2.replaceFirst("\\(", "")
+                        .replace("\"", "").split(","))
+                .map(x -> StringUtils.removeEnd(x, ")")).collect(Collectors.toList());
+        logger.info("parenthesesRemove: {}", parenthesesRemove);
+
+        String test3 = "a in (\"qqq\", \"qqw\") and b in (\"2332\")";
+        for (String x : Arrays.stream(test3.split(" and | or ")).collect(Collectors.toList())) {
+            String[] split = x.split(" in ");
+            String[] split1 = split[1].replaceAll("\\(|\\)| |\"", "").split(",");//NOSONAR
+            List<String> parseFilter = Arrays.stream(split1).collect(Collectors.toList());
+            logger.info("parseFilter: {}", parseFilter);
+        }
+
         String s = testReg("2020-02-22 00:00:00");
         logger.info("{}", s);
         String regex1 = "FR|RB|Residual";
